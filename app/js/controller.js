ec349c08f348970c7defb543039fea3693dad137
Quentin Machard <quentin.machard@gmail.com>
2015-04-04 18:33:15 +0200

    Affichage de données statiques

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,16 +1,48 @@
 var horariosApp = angular.module('horariosApp', []);
 
-horariosApp.controller('StationsCtrl', function($scope) {
+horariosApp.controller('StationsCtrl', function($scope, $http) {
+	$http.get('todo.php').success(function(data) {
+
+	});
+
+
 	$scope.stations = [
 		{
-			'name':'Basse-Chenaie',
-			'line': 'C7',
-			'terminus': 'Souillarderie'
+			code: 'BCHE2',
+			name:'Basse-Chenaie',
+			line: 'C7',
+			terminus: 'Souillarderie',
+			times: [
+				'Proche',
+				'2 mn',
+				'13 mn'
+			]
 		},
 		{
-			'name':'Vison',
-			'line': '12',
-			'terminus': 'Jules Vernes'
+			code: 'VISO1',
+			name:'Vison',
+			line: '12',
+			terminus: 'Jules Vernes',
+			times: [
+				'Proche',
+				'4 mn',
+				'25 mn'
+			]
 		}
 	];
+
+	var isUpdating = false;
+	$scope.updateTimestable = function(haveLoader) {
+		// If is loading new timesTable exit
+		if(isUpdating) return;
+		isUpdating = true;
+
+		// If must be a loader
+		haveLoader = (typeof haveLoader === 'undefined') ? false : haveLoader;
+
+		$scope.stations[0].times[0] = '0 mn';
+		isUpdating = false;
+	};
+
+	setInterval($scope.updateTimestable, 10000);
 });
